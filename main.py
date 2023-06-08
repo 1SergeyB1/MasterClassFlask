326cb88a0e46d76aaa406eae193ad54a52a56859
ASSword228 <89596664+ASSword228@users.noreply.github.com>
2023-06-09 01:32:57 +0800

    Завтра доделаю

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from requests import post
+import os
+
+IMG_FOLDER = os.path.join('jpeg')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Data.db'
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    status = db.Column(db.String)
+    species = db.Column(db.String)
+    Type = db.Column(db.String)
+    gender = db.Column(db.String)
+    created = db.Column(db.String)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    items = Data.query.order_by(Data.id).all()
+    return render_template('index.html', data = items)
+
+
+@app.route('/Create', methods=['POST', 'GET'])
+def Create():
+    return render_template('Edit.html')
+
+
+@app.route('/Edit', methods=['POST', 'GET'])
+def Edit():
+    return render_template('Create.html')
+
+@app.route("/images")
+def Display_IMG():
+    IMG_LIST = os.listdir('static/jpeg')
+    IMG_LIST = ['jpeg/' + i for i in IMG_LIST]
+    return render_template("images.html", imagelist=IMG_LIST)
+
+@app.route('/Character/<int:item_id>', methods=['POST', 'GET'])
+def Character(item_id):
+    item = db.session.get(Data, item_id)
+    img = os.path.join(app.config['UPLOAD_FOLDER'] + '/' + str(item_id) + '.jpeg')
+    return render_template('Character.html', data = item, img = img)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
