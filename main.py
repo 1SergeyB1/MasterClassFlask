da3c76c7d9320408ba7207ec2c6884903485809b
ASSword228 <89596664+ASSword228@users.noreply.github.com>
2023-06-10 13:26:08 +0800

    Финальная версия

--- main.py
+++ main.py
@@ -28,19 +28,60 @@ def index():
 
 @app.route('/Create', methods=['POST', 'GET'])
 def Create():
-    return render_template('Edit.html')
+    if request.method == "POST":
+        item = Data()
+        item.name = request.form['name']
+        item.status = request.form['status']
+        item.species = request.form['species']
+        item.Type = request.form['Type']
+        item.gender = request.form['gender']
+        db.session.add(item)
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return redirect('/Create')
+    img = os.path.join(app.config['UPLOAD_FOLDER'] + '/' + '-1' + '.jpeg')
+    return render_template('Create.html', img=img)
 
 
-@app.route('/Edit', methods=['POST', 'GET'])
-def Edit():
-    return render_template('Create.html')
+@app.route('/Edit/<int:item_id>', methods=['POST', 'GET'])
+def Edit(item_id):
+    if request.method == "POST":
+        item = db.session.get(Data, item_id)
+        item.name = request.form['name']
+        item.status = request.form['status']
+        item.species = request.form['species']
+        item.Type = request.form['Type']
+        item.gender = request.form['gender']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            print(item.email, item.nickname, item.password)
+            return redirect('/Edit/'+item_id)
+    item = db.session.get(Data, item_id)
+    img = os.path.join(app.config['UPLOAD_FOLDER'] + '/' + str(item_id) + '.jpeg')
+    return render_template('Edit.html', data = item, img=img)
 
 @app.route('/Character/<int:item_id>', methods=['POST', 'GET'])
 def Character(item_id):
+    if request.method == "POST":
+        delete = db.session.get(Data, item_id)
+        print(delete)
+        db.session.delete(delete)
+        db.session.commit()
+        try:
+            os.remove('./static/avatars/'+str(item_id))
+        except:
+            return redirect('/')
+        return redirect('/')
+
     item = db.session.get(Data, item_id)
     img = os.path.join(app.config['UPLOAD_FOLDER'] + '/' + str(item_id) + '.jpeg')
+    if os.path.exists('./static/'+img) == False:
+        img = '/avatars/-1.jpeg'
     return render_template('Character.html', data = item, img = img)
-
 
 if __name__ == "__main__":
     app.run(debug=False)
